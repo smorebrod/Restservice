41fd30084b3ecf63b4e5f0dff23abdfe7b7cf173
smorebrod <36259392+smorebrod@users.noreply.github.com>
2019-09-27 13:34:04 +0200

    rest

---
+++ RestServiceprooveiderMeassurement/RestServiceprooveiderMeassurement/Controllers/MeassurementsController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestServiceprooveiderMeassurement.Model;
+
+namespace RestServiceprooveiderMeassurement.Controllers
+{
+    public class MeassurementsController : Controller
+    {
+
+        private static List<Meassurement> mList = new List<Meassurement>()
+        {
+         new Meassurement(1,21,22,23),
+        new Meassurement(2,22,23,24)
+
+        };
+
+     
+      
+        
+
+        // GET: Meassurements
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Meassurements/Details/allid
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Meassurements/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Meassurements/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: APi/Meassurements/
+        public List<Meassurement> get()
+        {
+            return mList;
+        }
+
+        // POST: Meassurements/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Meassurements/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Meassurements/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ RestServiceprooveiderMeassurement/RestServiceprooveiderMeassurement/Model/Meassurement.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestServiceprooveiderMeassurement.Model
+{
+    public class Meassurement
+    {
+        public int Id { get; set; }
+
+        public int  Pressure { get; set; }
+
+        public int Humidity { get; set; }
+
+        public int Temperature { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public Meassurement()
+        {
+
+        }
+
+        public Meassurement(int id, int pressure, int humidity, int temperature)
+        {
+            this.Id = id;
+            this.Pressure = pressure;
+            this.Humidity = humidity;
+            this.Temperature = temperature;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{nameof(Pressure)}: {Pressure}, {nameof(Humidity)}: {Humidity}, {nameof(Temperature)}: {Temperature}";
+
+        }
+
+    }
+}
